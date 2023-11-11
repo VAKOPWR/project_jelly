af8fa4bf042cc27591833398c8da86e23748126b
Kr1SEl <k.kostakov2002@gmail.com>
2023-11-11 18:56:36 +0100

    color fix

--- lib/pages/ghost_mode/tabs/ghost_mode_tab_everyone.dart
+++ lib/pages/ghost_mode/tabs/ghost_mode_tab_everyone.dart
@@ -14,150 +14,149 @@ class _GhostModeTabEveryoneState extends State<GhostModeTabEveryone> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.white,
         child: Stack(
+      children: [
+        Column(
           children: [
-            Column(
+            Divider(
+              color: Colors.black,
+              height: 20.0,
+            ),
+            Text('Location precision visibility'),
+            Row(
               children: [
-                Divider(
-                  color: Colors.black,
-                  height: 20.0,
+                Radio<int>(
+                  value: 1,
+                  groupValue: locationPrecisionOption,
+                  onChanged: (value) {
+                    setState(() {
+                      locationPrecisionOption = value!;
+                    });
+                  },
                 ),
-                Text('Location precision visibility'),
-                Row(
-                  children: [
-                    Radio<int>(
-                      value: 1,
-                      groupValue: locationPrecisionOption,
-                      onChanged: (value) {
-                        setState(() {
-                          locationPrecisionOption = value!;
-                        });
-                      },
-                    ),
-                    Text('Precise'),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Radio<int>(
-                      value: 2,
-                      groupValue: locationPrecisionOption,
-                      onChanged: (value) {
-                        setState(() {
-                          locationPrecisionOption = value!;
-                        });
-                      },
-                    ),
-                    Text('City district'),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Radio<int>(
-                      value: 3,
-                      groupValue: locationPrecisionOption,
-                      onChanged: (value) {
-                        setState(() {
-                          locationPrecisionOption = value!;
-                        });
-                      },
-                    ),
-                    Text('City'),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Radio<int>(
-                      value: 4,
-                      groupValue: locationPrecisionOption,
-                      onChanged: (value) {
-                        setState(() {
-                          locationPrecisionOption = value!;
-                        });
-                      },
-                    ),
-                    Text('Hide my location'),
-                  ],
-                ),
-                Divider(
-                  color: Colors.black,
-                  height: 20.0,
-                ),
-                Text('Some other settings'),
-                Row(
-                  children: [
-                    Radio<int>(
-                      value: 1,
-                      groupValue: otherOption,
-                      onChanged: (value) {
-                        setState(() {
-                          otherOption = value!;
-                        });
-                      },
-                    ),
-                    Text('Option 1'),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Radio<int>(
-                      value: 2,
-                      groupValue: otherOption,
-                      onChanged: (value) {
-                        setState(() {
-                          otherOption = value!;
-                        });
-                      },
-                    ),
-                    Text('Option 2'),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Radio<int>(
-                      value: 3,
-                      groupValue: otherOption,
-                      onChanged: (value) {
-                        setState(() {
-                          otherOption = value!;
-                        });
-                      },
-                    ),
-                    Text('Option 3'),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Radio<int>(
-                      value: 4,
-                      groupValue: otherOption,
-                      onChanged: (value) {
-                        setState(() {
-                          otherOption = value!;
-                        });
-                      },
-                    ),
-                    Text('Option 4'),
-                  ],
-                ),
+                Text('Precise'),
               ],
             ),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                padding: EdgeInsets.all(10.0),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green[600]),
-                  child: Text('Confirm'),
+            Row(
+              children: [
+                Radio<int>(
+                  value: 2,
+                  groupValue: locationPrecisionOption,
+                  onChanged: (value) {
+                    setState(() {
+                      locationPrecisionOption = value!;
+                    });
+                  },
                 ),
-              ),
-            )
+                Text('City district'),
+              ],
+            ),
+            Row(
+              children: [
+                Radio<int>(
+                  value: 3,
+                  groupValue: locationPrecisionOption,
+                  onChanged: (value) {
+                    setState(() {
+                      locationPrecisionOption = value!;
+                    });
+                  },
+                ),
+                Text('City'),
+              ],
+            ),
+            Row(
+              children: [
+                Radio<int>(
+                  value: 4,
+                  groupValue: locationPrecisionOption,
+                  onChanged: (value) {
+                    setState(() {
+                      locationPrecisionOption = value!;
+                    });
+                  },
+                ),
+                Text('Hide my location'),
+              ],
+            ),
+            Divider(
+              color: Colors.black,
+              height: 20.0,
+            ),
+            Text('Some other settings'),
+            Row(
+              children: [
+                Radio<int>(
+                  value: 1,
+                  groupValue: otherOption,
+                  onChanged: (value) {
+                    setState(() {
+                      otherOption = value!;
+                    });
+                  },
+                ),
+                Text('Option 1'),
+              ],
+            ),
+            Row(
+              children: [
+                Radio<int>(
+                  value: 2,
+                  groupValue: otherOption,
+                  onChanged: (value) {
+                    setState(() {
+                      otherOption = value!;
+                    });
+                  },
+                ),
+                Text('Option 2'),
+              ],
+            ),
+            Row(
+              children: [
+                Radio<int>(
+                  value: 3,
+                  groupValue: otherOption,
+                  onChanged: (value) {
+                    setState(() {
+                      otherOption = value!;
+                    });
+                  },
+                ),
+                Text('Option 3'),
+              ],
+            ),
+            Row(
+              children: [
+                Radio<int>(
+                  value: 4,
+                  groupValue: otherOption,
+                  onChanged: (value) {
+                    setState(() {
+                      otherOption = value!;
+                    });
+                  },
+                ),
+                Text('Option 4'),
+              ],
+            ),
           ],
-        ));
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            padding: EdgeInsets.all(10.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Colors.green[600]),
+              child: Text('Confirm'),
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
