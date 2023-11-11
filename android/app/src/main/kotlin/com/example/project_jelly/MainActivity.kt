0efd6e09f81d01abed93b9f7292ffd632450ddb7
vi-mcw196 <moldlova50@gmail.com>
2023-11-11 19:45:54 +0100

    modify MainActivity.kt to handle getBatteryLevel

--- android/app/src/main/kotlin/com/example/project_jelly/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/project_jelly/MainActivity.kt
@@ -1,40 +1,33 @@
 package com.example.project_jelly
 
-import android.app.ActivityManager
-import android.content.Context
-import android.os.BatteryManager
-import android.os.Build
-import android.os.Process
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-import java.io.RandomAccessFile
+import android.content.pm.PackageManager
+import android.os.BatteryManager
+import android.content.Context
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
-class MainActivity : FlutterActivity() {
-    private val CHANNEL = "com.example.project_jelly/resourceUsage"
-
-    private val PHONE_STATE_PERMISSION_REQUEST_CODE = 1
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_PHONE_STATE),
-                    PHONE_STATE_PERMISSION_REQUEST_CODE)
-            }
-        }
-    }
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.project_jelly.battery_info_channel"
+    private val READ_PHONE_STATE_PERMISSION_CODE = 100
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-            if (call.method == "getResourceUsage") {
-                if (hasPhoneStatePermission()) {
-                    getResourceUsage(result)
+            if (call.method == "getBatteryLevel") {
+                if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_PHONE_STATE), READ_PHONE_STATE_PERMISSION_CODE)
                 } else {
-                    result.error("PERMISSION_DENIED", "Read phone state permission denied", null)
+                    val batteryLevel = getBatteryLevel()
+                    if (batteryLevel != -1) {
+                        result.success(batteryLevel)
+                    } else {
+                        result.error("UNAVAILABLE", "Battery level not available.", null)
+                    }
                 }
             } else {
                 result.notImplemented()
@@ -42,81 +35,21 @@ class MainActivity : FlutterActivity() {
         }
     }
 
-    private fun hasPhoneStatePermission(): Boolean {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
-                == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun getResourceUsage(result: MethodChannel.Result) {
-        if (hasPhoneStatePermission()) {
-            val batteryUsage = getBatteryUsage()
-            val memoryUsage = getMemoryUsage()
-            val cpuUsage = getCpuUsage()
-            val usageStats = mapOf(
-                    "batteryUsage" to batteryUsage,
-                    "memoryUsage" to memoryUsage,
-                    "cpuUsage" to cpuUsage
-            )
-            result.success(usageStats)
-        } else {
-            result.error("PERMISSION_DENIED", "Read phone state permission not granted", null)
-        }
-    }
-
-    private fun getBatteryUsage(): String {
+    private fun getBatteryLevel(): Int {
         val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
-        // Note: This API requires Android 6.0 (API level 23) or higher.
-        val batteryUsage = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val batteryPercent: Int = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
-            "Battery level: $batteryPercent%"
-        } else {
-            "Battery level not available on pre-Marshmallow devices."
+        return batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            READ_PHONE_STATE_PERMISSION_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    val batteryLevel = getBatteryLevel()
+                } else {
+                    // Permission denied
+                }
+                return
+            }
         }
-        return batteryUsage
-    }
-
-    private fun getMemoryUsage(): String {
-        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-        val memoryInfo = ActivityManager.MemoryInfo()
-        activityManager.getMemoryInfo(memoryInfo)
-        val totalMemory = memoryInfo.totalMem / (1024 * 1024)
-        val availMemory = memoryInfo.availMem / (1024 * 1024)
-        return "Total Memory: ${totalMemory}MB, Available Memory: ${availMemory}MB"
-    }
-
-    private fun getCpuUsage(): String {
-        try {
-            val pid = Process.myPid()
-            val cpuStatPath = "/proc/$pid/stat"
-            val reader = RandomAccessFile(cpuStatPath, "r")
-            val load = reader.readLine().split(" ")
-
-            val utime = load[13].toLong()
-            val stime = load[14].toLong()
-            val cutime = load[15].toLong()
-            val cstime = load[16].toLong()
-            val startTime = load[21].toLong()
-            val uptime = System.currentTimeMillis() / 10
-
-            val totalProcessTime = utime + stime + cutime + cstime
-            val totalTime = uptime - startTime
-
-            reader.close()
-
-            // The CPU usage calculation will be a rough estimation,
-            // as we are considering total time passed since the process start,
-            // which includes sleep time as well. Real CPU monitoring requires polling at intervals.
-            val cpuUsage = totalProcessTime / (totalTime.toFloat() * numberOfCores())
-
-            return "Approximate CPU usage: ${(cpuUsage * 100).toInt()}%"
-        } catch (e: Exception) {
-            return "Failed to read CPU usage: ${e.message}"
-        }
-    }
-
-    private fun numberOfCores(): Int {
-        return File("/sys/devices/system/cpu/").listFiles { pathname ->
-            Pattern.matches("cpu[0-9]+", pathname.name)
-        }?.size ?: 1
     }
 }
