97590d0a8737a993e0d949f4e35116217bf58f41
Kr1SEl <59517688+Kr1SEl@users.noreply.github.com>
2023-11-12 15:06:06 +0100

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:project_jelly/pages/auth/login.dart';
 import 'package:project_jelly/widgets/map.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,27 +16,34 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    if (Platform.isIOS) {
-      return Scaffold(
-        body: Stack(
-          children: [MapWidget()],
-        ),
-      );
+    if (FirebaseAuth.instance.currentUser == null) {
+      Future.delayed(Duration.zero, () {
+        Get.offNamed('/login');
+      });
+      return LogInPage();
     } else {
-      return Scaffold(
-        appBar: PreferredSize(
-          preferredSize:
-              Size.fromHeight(0), // Setting the height to 0 hides the app bar
-          child: AppBar(
-            backgroundColor: Theme.of(context)
-                .colorScheme
-                .primary, // Set the color of the app bar
+      if (Platform.isIOS) {
+        return Scaffold(
+          body: Stack(
+            children: [MapWidget()],
           ),
-        ),
-        body: Stack(
-          children: [MapWidget()],
-        ),
-      );
+        );
+      } else {
+        return Scaffold(
+          appBar: PreferredSize(
+            preferredSize:
+                Size.fromHeight(0), // Setting the height to 0 hides the app bar
+            child: AppBar(
+              backgroundColor: Theme.of(context)
+                  .colorScheme
+                  .primary, // Set the color of the app bar
+            ),
+          ),
+          body: Stack(
+            children: [MapWidget()],
+          ),
+        );
+      }
     }
   }
 }
