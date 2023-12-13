0745081463aad2cf5ca76b8607dcc32aed0b43a4
vi-mcw196 <moldlova50@gmail.com>
2023-12-13 21:07:32 +0100

    delete chat group logic

--- lib/pages/messages.dart
+++ lib/pages/messages.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:project_jelly/pages/chat/tabs/chat_friends_tab.dart';
-import 'package:project_jelly/pages/chat/tabs/chat_groups_tab.dart';
-import 'package:project_jelly/pages/chat/tabs/create_chat_group.dart';
 
 class MessagesPage extends StatefulWidget {
   const MessagesPage({super.key});
@@ -49,42 +46,17 @@ class _MessagesPageState extends State<MessagesPage>
               iconTheme: IconThemeData(
                 color: Theme.of(context).colorScheme.onPrimary,
               ),
-              actions: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: () {
-                    Get.to(() => CreateGroupChat());
-                  },
-                ),
-              ],
             ),
             body: Container(
               color: Theme.of(context).colorScheme.primary,
               child: Column(
                 children: [
-                  TabBar(
-                    controller: _tabController,
-                    tabs: [
-                      Tab(
-                          child: Text("Friends",
-                              style: TextStyle(
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .onPrimary))),
-                      Tab(
-                          child: Text("Groups",
-                              style: TextStyle(
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .onPrimary))),
-                    ],
-                  ),
                   Expanded(
                       child: Container(
                           color: Theme.of(context).colorScheme.background,
                           child: TabBarView(
                             controller: _tabController,
-                            children: [ChatFriendsTab(), ChatGroupsTab()],
+                            children: [ChatFriendsTab()],
                           )))
                 ],
               ),
